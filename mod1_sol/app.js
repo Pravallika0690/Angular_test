177b9df8390d8d15dc07beb3ad96cc32fe9ea089
Pravallika <pravallika0690@gmail.com>
2016-10-06 20:40:35 -0400

    mod1_sol/app.js

--- mod1_sol/app.js
+++ mod1_sol/app.js
@@ -3,6 +3,10 @@ var lc= angular.module("LunchCheck", []);
 lc.controller("LunchCheckController", ["$scope", function($scope) {
  $scope.splitString = function(stringToSplit, separator) {
 
+if(stringToSplit==undefined || separator== undefined) {
+	console.log("please enter valid items");
+}
+else {
  $scope.arrayOfStrings = stringToSplit.split(separator);
 
  console.log('The original string is: "' + stringToSplit + '"');
@@ -10,8 +14,8 @@ lc.controller("LunchCheckController", ["$scope", function($scope) {
   console.log('The array has ' + $scope.arrayOfStrings.length + ' elements: ' + $scope.arrayOfStrings.join(' / '));
 
  }
-var comma= ',';
+//var comma= ',';
 
-
+}
 }])
 })();
